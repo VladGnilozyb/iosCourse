05bcbd1a4f76200698e99bc82ffacbebbb90d581
Vlad <vlad.gnilozyb@gmail.com>
2017-12-06 14:20:31 +0300

    простейшие операции(Optional)

---
+++ classwork/operationEasy.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var MyString = ""
+if MyString.isEmpty{ //ggeстая строка
+    print ("empty")
+}
+
+
+let name = "REck"
+let age = 30
+print ("\(name) is \(age) years old")// or (name  + "is" + STring(30))
+
+let emptyString = "no empty"
+if emptyString.isEmpty{
+    print("There's nothing here")
+}
+else {
+    print("It's not as empty as I thought")
+}
+
+
