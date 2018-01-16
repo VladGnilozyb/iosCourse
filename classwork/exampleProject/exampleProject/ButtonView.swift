c65e95d0665d03a5bb6e1b5181c6864fb7228d56
Владислав Гнилозуб <vlad.gnilozyb@gmail.com>
2018-01-17 01:11:00 +0300

    sample project with delegate

---
+++ classwork/exampleProject/exampleProject/ButtonView.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonView.swift
+//  exampleProject
+//
+//  Created by Владислав Гнилозуб on 1/15/18.
+//  Copyright © 2018 Владислав Гнилозуб. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewButton: UIView {
+  
+    
+    
+}
